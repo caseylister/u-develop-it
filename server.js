539469055e2bbea819323673c0384b50b49eb501
caseylister <caseynlister@gmail.com>
2021-11-03 12:39:47 -0400

    query db to test connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'c511s731L',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 
 // Default response for any other request (Not Found)
@@ -14,6 +27,10 @@ app.use((req, res) => {
     res.status(404).end();
   });
 
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 
 app.listen(PORT, () => {
